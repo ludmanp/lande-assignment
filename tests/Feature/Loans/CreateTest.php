2e33f6dfb6fe39f3c9853b752ce2196e2cad80a0
Mark Leidman <marksleidmans@flynowpaylater.com>
2023-09-26 10:25:00 +0300

    Merged create endpoint tests

--- tests/Feature/Loans/CreateTest.php
+++ tests/Feature/Loans/CreateTest.php
@@ -50,4 +50,146 @@ class CreateTest extends TestCase
             ])
         );;
     }
+
+    /**
+     * @dataProvider provideRequests
+     */
+    public function testIncorrectRequestFail($requestData, $expectedContent): void
+    {
+        $response = $this->postJson('/api/loan', $requestData);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+        $this->assertEquals($expectedContent, $content);
+    }
+
+    public static function provideRequests()
+    {
+        return [
+            [
+                [
+                    'amountInCents' => 'hundred',
+                    'term' => 12,
+                    'interestRateInBasisPoints' => 400,
+                    'euriborRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The amount in cents field must be an integer.",
+                    "errors" => [
+                        "amountInCents" => [
+                            "The amount in cents field must be an integer."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'term' => 12,
+                    'interestRateInBasisPoints' => 400,
+                    'euriborRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The amount in cents field is required.",
+                    "errors" => [
+                        "amountInCents" => [
+                            "The amount in cents field is required."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'amountInCents' => 100000,
+                    'term' => 'twelve',
+                    'interestRateInBasisPoints' => 400,
+                    'euriborRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The term field must be an integer.",
+                    "errors" => [
+                        "term" => [
+                            "The term field must be an integer."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'amountInCents' => 100000,
+                    'interestRateInBasisPoints' => 400,
+                    'euriborRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The term field is required.",
+                    "errors" => [
+                        "term" => [
+                            "The term field is required."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'amountInCents' => 1000000,
+                    'term' => 12,
+                    'interestRateInBasisPoints' => '4,3',
+                    'euriborRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The interest rate in basis points field must be an integer.",
+                    "errors" => [
+                        "interestRateInBasisPoints" => [
+                            "The interest rate in basis points field must be an integer."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'amountInCents' => 100000,
+                    'term' => 12,
+                    'euriborRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The interest rate in basis points field is required.",
+                    "errors" => [
+                        "interestRateInBasisPoints" => [
+                            "The interest rate in basis points field is required."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'amountInCents' => 1000000,
+                    'term' => 12,
+                    'interestRateInBasisPoints' => 400,
+                    'euriborRateInBasisPoints' => 3.94
+                ],
+                [
+                    "message" => "The euribor rate in basis points field must be an integer.",
+                    "errors" => [
+                        "euriborRateInBasisPoints" => [
+                            "The euribor rate in basis points field must be an integer."
+                        ]
+                    ]
+                ]
+            ],
+            [
+                [
+                    'amountInCents' => 100000,
+                    'term' => 12,
+                    'interestRateInBasisPoints' => 394
+                ],
+                [
+                    "message" => "The euribor rate in basis points field is required.",
+                    "errors" => [
+                        "euriborRateInBasisPoints" => [
+                            "The euribor rate in basis points field is required."
+                        ]
+                    ]
+                ]
+            ],
+        ];
+    }
 }
